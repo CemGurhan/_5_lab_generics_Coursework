61a009ccf701e8c8312eb6d6277fe87f369d0151
Cem Gurhan <cemg@live.co.uk>
2022-04-27 15:57:22 +0100

    experimented with some class methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
-package SimpleClass;
+import ClassParams2Class.Pair;
+import SimpleClass.Box;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,15 @@ public class Main {
 
         System.out.println("Box with integer input only output: " + boxForIntegersOnly.get());
         System.out.println("Box without restrictions output: " + boxWithoutRestrictions.get());
+        System.out.println("");
+
+        Pair<String, Integer> pair1 = new Pair("W",1);
+        Pair<Integer,String> pair2 = new Pair(1, "W");
+        Pair<String , String> pair3 = new Pair("W","WW.");
+
+        System.out.println("pair1: "+ pair1.getK()+" "+pair1.getV());
+        System.out.println("pair3: "+ pair3.getK() + " "+ pair3.getV());
+
 
 
 
