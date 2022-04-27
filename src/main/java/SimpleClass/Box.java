29df8483547afeb23430d81ec98fb892827a53ce
Cem Gurhan <cemg@live.co.uk>
2022-04-27 15:11:19 +0100

    added a private instance variable called t, of type T. Added a get method,returning the value of variable t, of type T. added a constructor, that accepts a variable of type T as its parameter.

--- src/main/java/SimpleClass/Box.java
+++ src/main/java/SimpleClass/Box.java
@@ -1,2 +1,16 @@
-package SimpleClass;public class Box {
+package SimpleClass;
+
+public class Box<T> {
+
+    private T t;
+
+    public Box(T t){
+        this.t=t;
+    }
+
+    public T get(){
+        return this.t;
+    }
+
+
 }
