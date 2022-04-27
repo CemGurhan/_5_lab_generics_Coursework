eda522c1c6b68ad52ec27e0adc75031a4bae3e28
Cem Gurhan <cemg@live.co.uk>
2022-04-27 16:02:39 +0100

    Added methods to Interface and created StringAndInteger class

--- src/main/java/InterfaceNeeded/GenericInterface.java
+++ src/main/java/InterfaceNeeded/GenericInterface.java
@@ -1,4 +1,14 @@
 package InterfaceNeeded;
 
 public interface GenericInterface {
+
+    Integer doSomeOperations();
+
+    String doReverseOperations();
+
+
+
+
+
+
 }
---
+++ src/main/java/InterfaceNeeded/StringAndInteger.java
@@ -0,0 +1,4 @@
+package InterfaceNeeded;
+
+public class StringAndInteger {
+}
